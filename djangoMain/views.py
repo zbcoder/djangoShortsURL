564f425fe82a416c13e291b25de028b8b92a6a8d
zbcoder <zburlov0303@mail.ru>
2022-12-09 19:46:22 +0500

    final commit

--- djangoMain/views.py
+++ djangoMain/views.py
@@ -33,18 +33,18 @@ def create_link(request):
         full_url = request.POST.get('full_url')
         cut_url = request.POST.get('cut_url')
         if cut_url[-1]=='/':
-            cut_url = cut_url[1: -1]
-        print(request.user.id, cut_url)
+            cut_url = '/link/'+cut_url[0: -1]+'/'
+        else:
+            cut_url = '/link/'+cut_url+'/'
         data = json.loads(serializers.serialize('json', Links.objects.filter(user_id = request.user.id)))
         try:
             for record in data:
-                print(record['fields']['cut_url'].split('/')[-2], cut_url)
-                if record['fields']['cut_url'].split('/')[-2] == cut_url:
+                if record['fields']['cut_url'] == cut_url:
                     errors = 'Такая сокращенная ссылка уже существует! Введите другую'
                     return render(request, template_name='profile/links.html', context={'user_links': data, 'errors': errors, 'full_url': full_url})
             Links.objects.create(
                 full_url = full_url,
-                cut_url = '/link/'+cut_url+'/',
+                cut_url = cut_url,
                 user_id = User.objects.get(pk=request.user.id).id
             ).save()
             return redirect('create_link')
